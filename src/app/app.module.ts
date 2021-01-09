9b3f73a04a43d6cb2274c055e7e842fb1f9fd61b
vishalmanes109 <36571896+vishalmanes109@users.noreply.github.com>
2021-01-09 19:42:09 +0530

    multiple dropdown

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,23 +1,24 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { RegisterComponent } from './users/register/register.component';
-import { LoginComponent } from './users/login/login.component';
-import { NavbarComponent } from './core/navbar/navbar.component';
-import { AboutComponent } from './core/about/about.component';
-import { ContactComponent } from './core/contact/contact.component';
-import { ProfileComponent } from './users/profile/profile.component';
-import { UpdateprofileComponent } from './users/updateprofile/updateprofile.component';
-import { CreatepostComponent } from './post/createpost/createpost.component';
-import { UpdatepostComponent } from './post/updatepost/updatepost.component';
-import { MyfeedComponent } from './postlist/myfeed/myfeed.component';
-import { CreateprofileComponent } from './users/createprofile/createprofile.component';
-import { PagenotfoundComponent } from './core/pagenotfound/pagenotfound.component';
-import { FooterComponent } from './core/footer/footer.component';
-import { HttpClientModule } from '@angular/common/http';
-
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { RegisterComponent } from "./users/register/register.component";
+import { LoginComponent } from "./users/login/login.component";
+import { NavbarComponent } from "./core/navbar/navbar.component";
+import { AboutComponent } from "./core/about/about.component";
+import { ContactComponent } from "./core/contact/contact.component";
+import { ProfileComponent } from "./users/profile/profile.component";
+import { UpdateprofileComponent } from "./users/updateprofile/updateprofile.component";
+import { CreatepostComponent } from "./post/createpost/createpost.component";
+import { UpdatepostComponent } from "./post/updatepost/updatepost.component";
+import { MyfeedComponent } from "./postlist/myfeed/myfeed.component";
+import { CreateprofileComponent } from "./users/createprofile/createprofile.component";
+import { PagenotfoundComponent } from "./core/pagenotfound/pagenotfound.component";
+import { FooterComponent } from "./core/footer/footer.component";
+import { HttpClientModule } from "@angular/common/http";
+import { NgMultiSelectDropDownModule } from "ng-multiselect-dropdown";
+import { FormsModule } from "@angular/forms";
 
 @NgModule({
   declarations: [
@@ -34,15 +35,16 @@ import { HttpClientModule } from '@angular/common/http';
     MyfeedComponent,
     CreateprofileComponent,
     PagenotfoundComponent,
-    FooterComponent
+    FooterComponent,
   ],
   imports: [
     BrowserModule,
     HttpClientModule,
     AppRoutingModule,
+    NgMultiSelectDropDownModule.forRoot(),
+    FormsModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
-
+export class AppModule {}
--- src/app/users/createprofile/createprofile.component.ts
+++ src/app/users/createprofile/createprofile.component.ts
@@ -1,15 +1,66 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { IDropdownSettings } from "ng-multiselect-dropdown";
 
 @Component({
-  selector: 'app-createprofile',
-  templateUrl: './createprofile.component.html',
-  styleUrls: ['./createprofile.component.css']
+  selector: "app-createprofile",
+  templateUrl: "./createprofile.component.html",
+  styleUrls: ["./createprofile.component.css"],
 })
 export class CreateprofileComponent implements OnInit {
+  dropdownList = [];
+  selectedItems = [];
+  dropdownSettings: IDropdownSettings = {};
 
-  constructor() { }
+  constructor() {
+    this.dropdownList = [
+      { item_id: 1, item_text: "Mumbai" },
+      { item_id: 2, item_text: "Bangaluru" },
+      { item_id: 3, item_text: "Pune" },
+      { item_id: 4, item_text: "Navsari" },
+      { item_id: 5, item_text: "New Delhi" },
+    ];
+    this.selectedItems = [
+      { item_id: 3, item_text: "Pune" },
+      { item_id: 4, item_text: "Navsari" },
+    ];
+    this.dropdownSettings = {
+      singleSelection: false,
+      idField: "item_id",
+      textField: "item_text",
+      selectAllText: "Select All",
+      unSelectAllText: "UnSelect All",
+      itemsShowLimit: 3,
+      allowSearchFilter: true,
+    };
+  }
 
   ngOnInit(): void {
+    this.dropdownList = [
+      { id: 3, text: "Barbecue" },
+      { id: 4, text: "Mustard" },
+      { id: 5, text: "Ketchup" },
+      { id: 6, text: "Mayonaise" },
+    ];
+    this.selectedItems = [
+      { id: 1, text: "Fantasy" },
+      { id: 2, text: "Fiction" },
+    ];
+    this.dropdownSettings = {
+      singleSelection: false,
+      idField: "id",
+      textField: "text",
+      selectAllText: "Select All",
+      unSelectAllText: "UnSelect All",
+      itemsShowLimit: 3,
+      allowSearchFilter: true,
+      limitSelection: 2,
+    };
+  }
+  onItemSelect(item: any) {
+    console.log(item);
+  }
+  onSelectAll(items: any) {
+    console.log(items);
   }
 
 }
