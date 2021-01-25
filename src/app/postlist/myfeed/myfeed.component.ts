9d89c8a862e8de3c38dc2a8b49da1683e9a09c3f
vishalmanes109 <vishalmanes109@gmail.com>
2021-01-25 17:49:22 +0530

    fixed myfeed bugs

--- src/app/postlist/myfeed/myfeed.component.ts
+++ src/app/postlist/myfeed/myfeed.component.ts
@@ -36,88 +36,10 @@ export class MyfeedComponent implements OnInit {
         //  console.log(res.message);
         this.nearByPost = res.message;
         this.isDataFetch = true;
-        //  this.entirePostData = {
-        //   username: this.nearByPost.name,
-        //   title: this.nearByPost.title,
-        //   description: this.nearByPost.description,
-        //   date: this.nearByPost.post_time,
-        //   distace: this.nearByPost.distance,
-        //   profileId: this.nearByPost.profileid,
-        //   postId: this.nearByPost.postid,
-        //   giveBook: this.nearByPost.givebookname,
-        //   giveBookAuthor: this.nearByPost.givebookauthor,
-        //   takeBook: null,
-        //   takeBookAuthor: null,
-        // };
-        // this.entirePostData = {
-        //   rawData: this.nearByPost,
-        //   giveBook: null,
-        //   giveBookAuthor: null,
-        //   takeBook: null,
-        //   takeBookAuthor: null,
-        // };
-        //console.log("before:", this.entirePostData);
-
-        //console.log(this.nearByPost.length);
-        this.entirePostData = {
-          username: this.nearByPost.name,
-          title: this.nearByPost.title,
-          description: this.nearByPost.description,
-          date: this.nearByPost.post_time,
-          distace: this.nearByPost.distance,
-          profileId: this.nearByPost.profileid,
-          postId: this.nearByPost.postid,
-          giveBook: this.nearByPost.givebookname,
-          giveBookAuthor: this.nearByPost.givebookauthor,
-          takeBook: null,
-          takeBookAuthor: null,
-        };
-        for (this.i = 0; this.i < this.nearByPost.length; this.i++) {
-          //console.log("ji")
-          if (this.nearByPost[this.i].take_book_id) {
-            //console.log("Hi");
-            // console.log(this.nearByPost[this.i].take_book_id);
-
-            await this.takeBookData.push(
-              this.postService.getTakeBook(this.nearByPost[this.i].take_book_id)
-            );
-            forkJoin(this.takeBookData).subscribe(
-              async (res) => {
-                //console.log("res lol",res);
-                this.takeBookResult = res;
-                console.log(this.takeBookResult);
-                // for (let j = 0; j < this.nearByPost.length; j++) {
-                //   this.k = 0;
-
-                //   if (
-                //     this.k < this.takeBookResult.length &&
-                //     this.nearByPost[j].take_book_id ==
-                //       this.takeBookResult[this.k].message[0].id
-                //   ) {
-                //     console.log(
-                //       this.takeBookResult[this.k].message[0],
-                //       this.nearByPost[j]
-                //     );
-                //     this.entirePostData.takeBook = this.takeBookResult[
-                //       this.k
-                //     ].message[0];
-
-                //     this.k++;
-                //   }
-                // }
-              },
-              (err) => console.log(err)
-            );
-            await console.log(this.takeBookResult);
-          } else {
-            this.isBookNotAvailable = true;
-            this.takeBook = "Book is not specified by user";
-            this.takeBookAuthor = "Book author is not specified by user";
-            this.note = "kindly reach out to user by clicking on chat button";
-          }
-        }
+        console.log(this.nearByPost);
       },
       (err) => {
+        
         console.log(err);
       }
     );
