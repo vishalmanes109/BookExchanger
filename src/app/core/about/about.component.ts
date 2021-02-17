20b49431dd0324ff0d0885ae59b26c0012fd41b2
vishalmanes109 <vishalmanes109@gmail.com>
2021-02-17 22:22:32 +0530

    core pages added

--- src/app/core/about/about.component.ts
+++ src/app/core/about/about.component.ts
@@ -7,9 +7,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AboutComponent implements OnInit {
 
+  public what;
+  public how;
+  public why;
+
   constructor() { }
 
   ngOnInit(): void {
-  }
+    this.what =
+      'BookXchanger is the place to swap/exchange/trade your books. Our goal is to allow people to easily trade books that have been collecting dust on their bookshelves for books they want for free.';
+  this.how="Simple and Easy";
+  this.why="Well, let see why"
+    }
 
 }
