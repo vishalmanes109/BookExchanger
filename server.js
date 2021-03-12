fb1753d5b384bd2d5380b89a1710ec4ada3f976d
vishalmanes109 <vishalmanes109@gmail.com>
2021-03-12 14:18:41 +0530

    deploying on heroku

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 const express = require("express");
 const path = require("path");
 const app = express();
-app.use(express.static(__dirname + "/dist/book-exchanger"));
+app.use(express.static(__dirname + "/dist/BookExchanger"));
 app.get("/*", function (req, res) {
-  res.sendFile(path.join(__dirname + "/dist/book-exchanger/index.html"));
+  res.sendFile(path.join(__dirname + "/dist/BookExchanger/index.html"));
 });
 app.listen(process.env.PORT || 8080);
 
