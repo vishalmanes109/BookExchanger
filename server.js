7d769a581d7b4492cbb2df5aec9f48f57382359b
vishalmanes109 <vishalmanes109@gmail.com>
2021-03-12 13:55:56 +0530

    deploying on heroku

--- server.js
+++ server.js
@@ -5,4 +5,4 @@ app.use(express.static(__dirname + "/dist/BookExchanger"));
 app.get("/*", function (req, res) {
   res.sendFile(path.join(__dirname + "/dist/BookExchanger/index.html"));
 });
-app.listen(process.env.PORT || 8080);
+app.listen(process.env.PORT || 4200);
